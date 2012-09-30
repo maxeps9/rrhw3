b056ca5111cd144951876cbdfece204c3b71f3c9
Max Epelbaum <maxepe@seas.upenn.edu>
2012-09-30 19:56:32 -0400

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rrhw3::Application.config.secret_token = '6e06d9510430baf104d074a440cf60d1e84dbdf8f602427a7b65ddcb67a767e8d5bc686e98b3b0ebe3d9c801c77feef4c2a618284381cb512522ef79fdf845ef'
